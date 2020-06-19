e1d934167b83dfbe559f9c03d0bdb5badc1e71cc
Boris Saavedra <boris.saavedra@gmail.com>
2020-06-19 17:22:14 -0400

    First commit

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from . import main
+from flask import render_template, redirect, url_for
+
+@main.route("/")
+def index():
+    return redirect(url_for(".login"))
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+@main.route("/signin")
+def signin():
+    return render_template("signin.html")
